a7ea2f1d68f22411cec1a21d013ac2b4794a34b3
Freatnor <jonathan.randall.taylor@gmail.com>
2016-08-15 14:02:20 -0400

    intitial project commit to repository for project structure and package name

---
+++ app/src/main/java/com/jonathan/james/eric/project_3/SectionPageAdapterActivity.java
@@ -0,0 +1,15 @@
+package com.jonathan.james.eric.project_3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SectionPageAdapterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_section_page_adapter);
+
+        //ToDo add code for setting up Fragment Pager Adapter
+    }
+}
