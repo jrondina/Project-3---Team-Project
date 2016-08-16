d79b1b8a2126c4a91058b4caddc16dea5e95bf5e
James <jamesrondina@gmail.com>
2016-08-16 18:49:52 -0400

    adding fb dependencies

--- app/src/main/java/com/jonathan/james/eric/project_3/SectionPageAdapterActivity.java
+++ app/src/main/java/com/jonathan/james/eric/project_3/SectionPageAdapterActivity.java
@@ -3,12 +3,17 @@ package com.jonathan.james.eric.project_3;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
 public class SectionPageAdapterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_section_page_adapter);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
 
         //ToDo add code for setting up Fragment Pager Adapter
 
