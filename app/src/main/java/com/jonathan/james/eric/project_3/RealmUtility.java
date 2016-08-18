51bd4634e9efd108b4381407ae217001b3cd964c
eric.kivet <eric.kivet@gmail.com>
2016-08-18 17:59:11 -0400

    added init userPrefs

--- app/src/main/java/com/jonathan/james/eric/project_3/RealmUtility.java
+++ app/src/main/java/com/jonathan/james/eric/project_3/RealmUtility.java
@@ -294,6 +294,78 @@ public class RealmUtility implements Closeable{
     }
     */
 
+    private void initUserPrefs(){
+        UserPreferences userPreferences = new UserPreferences();
+        RealmList<Section> sections = new RealmList<>();
+        Section home = new Section();
+        home.setActive(true);
+        home.setKey("home");
+        home.setSectionName("Top News");
+        sections.add(home);
+
+        Section world = new Section();
+        world.setActive(true);
+        world.setKey("world");
+        world.setSectionName("World");
+        sections.add(world);
+
+        Section tech = new Section();
+        tech.setActive(true);
+        tech.setKey("technology");
+        tech.setSectionName("Tech");
+        sections.add(tech);
+
+        Section business = new Section();
+        business.setActive(true);
+        business.setKey("business");
+        business.setSectionName("Business");
+        sections.add(business);
+
+        Section politics = new Section();
+        politics.setActive(true);
+        politics.setKey("politics");
+        politics.setSectionName("Politics");
+        sections.add(politics);
+
+        Section science = new Section();
+        science.setActive(true);
+        science.setKey("science");
+        science.setSectionName("Science");
+        sections.add(science);
+
+        Section sports = new Section();
+        sports.setActive(true);
+        sports.setKey("sports");
+        sports.setSectionName("Sports");
+        sections.add(sports);
+
+        Section movies = new Section();
+        movies.setActive(true);
+        movies.setKey("movies");
+        movies.setSectionName("Movies");
+        sections.add(movies);
+
+        Section bookmarks = new Section();
+        bookmarks.setActive(true);
+        bookmarks.setKey("bookmarks");
+        bookmarks.setSectionName("Bookmarks");
+        sections.add(bookmarks);
+
+        RealmList<Source> sources = new RealmList<>();
+        Source nytimes = new Source();
+        nytimes.setName("New York Times");
+        nytimes.setActive(true);
+        sources.add(nytimes);
+
+        userPreferences.setSectionList(sections);
+        userPreferences.setSourceList(sources);
+        userPreferences.setUserName("default");
+
+        insertUserPreferences(userPreferences);
+
+
+    }
+
 
 
     @Override
